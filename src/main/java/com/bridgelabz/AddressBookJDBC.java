b8685b39a2461e9c0b77245fcee377039815f0e9
Swapnil639 <swapnilpatil631997@gmail.com>
2023-05-18 08:46:54 +0530

    Added :Welcome message

---
+++ src/main/java/com/bridgelabz/AddressBookJDBC.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class AddressBookJDBC {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program on JDBC ...");
+
+    }
+}
