eeaecc9c184dea59542abebd2ea64c799e5e502a
Swapnil639 <swapnilpatil631997@gmail.com>
2023-05-18 08:49:33 +0530

    Added :Connect the databse to java and retrive the data from the table

--- src/main/java/com/bridgelabz/AddressBookJDBC.java
+++ src/main/java/com/bridgelabz/AddressBookJDBC.java
@@ -1,8 +1,34 @@
 package com.bridgelabz;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class AddressBookJDBC {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program on JDBC ...");
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/address_book_service", "root", "Swapnil123");
+            System.out.println("Connection Done...");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from address_book");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1)
+                        + " | " + resultSet.getString(2)
+                        + " | " + resultSet.getString(3)
+                        + " | " + resultSet.getString(4)
+                        + " | " + resultSet.getString(5)
+                        + " | " + resultSet.getString(6)
+                        + " | " + resultSet.getInt(7)
+                        + " | " + resultSet.getInt(8)
+                        + " | " + resultSet.getString(9)
+                        + " | " + resultSet.getString(10));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
